5b27740014be7ad4dc3824b98da4462c35b11678
dsu01 <shan@THREEGIRLSHOME>
2017-03-11 10:51:24 -0500

    add new code

---
+++ MVCInventory.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCInventory.Domain.Abstract;
+using MVCInventory.Domain;
+
+namespace MVCInventory.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IBuildingRepository buildingRepo;
+
+        public AdminController(IBuildingRepository buildrepo)
+        {
+            buildingRepo = buildrepo;
+        }
+        public ActionResult ChangeBuildingName(string oldName, string newName)
+        {
+            Building building = buildingRepo.FetchByBuildingName(oldName);
+            building.BuildingName = newName;
+            buildingRepo.SubmitChanges();
+            return View();
+        }
+        
+    }
+}
