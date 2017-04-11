a86419a93c4ecf2b87177cd6a97e51c8d2298adc
David Su <dsu@digitalglobe.com>
2017-04-11 17:56:37 -0400

    basic unit tests

---
+++ Dave/MVCInventory.Testables/GreatWallClass.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCInventory.Testables
+{
+    public interface IGreatWallService
+    {
+        int MyIntProperty { get; set; }
+
+        string MyStringProperty { get; set; }
+
+        List<int> MyListProperty { get; set; }
+
+        void Init();
+
+        int Process(int num);
+    }
+
+    public class GreatWallClass
+    {
+        private readonly IGreatWallService _service;
+
+        public GreatWallClass(IGreatWallService service)
+        {
+            _service = service;
+        }
+
+        public bool Setup()
+        {
+            var returnVal = false;
+
+            try
+            {
+                _service.Init();
+                returnVal = true;
+            }
+            catch
+            {
+                returnVal = false;
+            }
+
+            return returnVal;
+        }
+
+        public bool SetupTwice()
+        {
+            var returnVal = false;
+
+            try
+            {
+                _service.Init();
+                _service.Init();
+                returnVal = true;
+            }
+            catch
+            {
+                returnVal = false;
+            }
+
+            return returnVal;
+        }
+
+        public int GetMyIntProperty()
+        {
+            return _service.MyIntProperty;
+        }
+
+        public string GetMyStringProperty()
+        {
+            return _service.MyStringProperty;
+        }
+
+        public List<int> GetMyListProperty()
+        {
+            return _service.MyListProperty;
+        }
+
+        public void Process()
+        {
+            _service.Process(10);
+        }
+    }
+}
---
+++ Dave/MVCInventory.UnitTests/GreatWallTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MVCInventory.Testables;
+
+namespace MVCInventory.UnitTests
+{
+    [TestClass]
+    public class GreatWallTest
+    {
+        private Dictionary<Type, object> _mocks = new Dictionary<Type, object>();
+
+        protected Mock<T> InitMock<T>() where T : class
+        {
+            var mock = new Mock<T>();
+            _mocks[typeof(T)] = mock;
+
+            return mock;
+        }
+
+        protected Mock<T> GetMock<T>() where T : class
+        {
+            return (Mock<T>)_mocks[typeof(T)];
+        }
+
+        [TestInitialize]
+        public void InitDependencies()
+        {
+            var service = this.InitMock<IGreatWallService>();
+
+            service.Setup(x => x.MyIntProperty).Returns(1);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var cut = new GreatWallClass(GetMock<IGreatWallService>().Object);
+
+            var result = cut.GetMyIntProperty();
+
+            Assert.IsTrue(result == 1);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var service = GetMock<IGreatWallService>();
+
+            var cut = new GreatWallClass(service.Object);
+
+            cut.Setup();
+
+            service.Verify(s => s.Init(), Times.Once);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var service = GetMock<IGreatWallService>();
+
+            var cut = new GreatWallClass(service.Object);
+
+            cut.Process();
+
+            service.Verify(s => s.Process(10), Times.Once);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var service = GetMock<IGreatWallService>();
+
+            var cut = new GreatWallClass(service.Object);
+
+            cut.Process();
+
+            service.Verify(s => s.Process(It.IsNotIn<int>(10)), Times.Never);
+        }
+    }
+}
